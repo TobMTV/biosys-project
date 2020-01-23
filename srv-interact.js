c767b8abbcaecc2def4be67a325f58255fb4a185
casalinovalerio <casalinovalerio.cv@gmail.com>
2020-01-23 13:05:48 +0100

    Add function recoFace() in srv-interact.js
    Co-authored-by: shaahin-sabeti <58465988+shaahin-sabeti@users.noreply.github.com>
    Co-authored-by: TobMTV <vendrame.tobia@gmail.com>

--- srv-interact.js
+++ srv-interact.js
@@ -1,7 +1,7 @@
 function sendFace() {
+    // Getting image from canvas to base64
     var picture = new Image();
     var canvas = document.getElementById("canvasOutput");
-
     picture.id = "pic";
     picture.src = canvas.toDataURL();
 
@@ -9,8 +9,28 @@ function sendFace() {
     var login = document.getElementById("login");
     var id = login.value;
 
+    // Actual request
     xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://biosys.casalinovalerio.com/send-faces.php', true);
+    xhr.send("image=" + picture.src + "&id=" + id );
+
+    alert(xhr.response);
+}
+
+function recoFace() {
+    // Getting image from canvas to base64
+    var picture = new Image();
+    var canvas = document.getElementById("canvasOutput");
+    picture.id = "pic";
+    picture.src = canvas.toDataURL();
+
+    // Placeholder
+    var login = document.getElementById("login");
+    var id = login.value;
+
+    // Actual request
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://biosys.casalinovalerio.com/reco-faces.php', true);
     xhr.send("image=" + picture.src + "&id=" + id );
 
     alert(xhr.response);
